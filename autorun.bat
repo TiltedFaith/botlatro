ea6d08db88b5359b15601b2c0ec9d840655e6c67
PolychromeOnion <gianmarcusalejandro@gmail.com>
2025-05-02 23:00:58 +0800

    Added Main Files

---
+++ autorun.bat
@@ -0,0 +1,20 @@
+@echo off
+:check_connection
+ping www.google.com -n 1 >nul 2>nul
+if %errorlevel% neq 0 (
+    echo No internet connection. Retrying in 30 seconds...
+    timeout /t 30
+    goto check_connection
+)
+
+:loop
+cd C:\sauce_bot.py
+python sauce_bot.py
+if %errorlevel% neq 0 (
+    echo Script failed. Restarting in 60 seconds...
+    timeout /t 60
+    goto loop
+) else (
+    echo Script completed successfully.
+)
+pause
